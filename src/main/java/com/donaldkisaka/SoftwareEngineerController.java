7f244752a236f10604b9c12b9c0d8658bb91fc51
DonaldKisaka <kisakadonald1@gmail.com>
2025-09-02 16:36:19 +0300

    software engineers

--- src/main/java/com/donaldkisaka/SoftwareEngineerController.java
+++ src/main/java/com/donaldkisaka/SoftwareEngineerController.java
@@ -9,19 +9,14 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/software-engineers")
 public class SoftwareEngineerController {
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
+
     @GetMapping
     public List<SoftwareEngineer> getEngineers() {
-      return List.of(
-              new SoftwareEngineer(
-                      1,
-                      "Donald",
-                      "js, typescript, nextjs, mongodb, tailwindcss"
-              ),
-              new SoftwareEngineer(
-                      2,
-                      "Ted",
-                      "java, spring, springboot"
-              )
-      );
+              return softwareEngineerService.getAllSoftwareEngineers();
     }
 }
